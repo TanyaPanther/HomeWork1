a9b15358fdc5c7ce9498bebd3baed87360389406
Tanya Panther <tanya.panther@mail.ru>
2023-02-16 20:57:53 +0900

    homework 2

---
+++ HW2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int a, b, c;
+ 
+Console.WriteLine("Введите три числа : ");
+ 
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+ 
+int max = a;
+if (b > max) 
+{
+    Console.WriteLine("{0} наибольшее число", b);  
+}   
+if(c > max)
+{   
+    Console.WriteLine("{0} наибольшее число ", c);   
+}   
+
+Console.WriteLine(max);
