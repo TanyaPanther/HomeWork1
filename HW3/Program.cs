fc26cd9b53d6fcf7c9b8c234b8c5e04a75afb1c8
Tanya Panther <tanya.panther@mail.ru>
2023-02-16 20:59:30 +0900

    homework 3

---
+++ HW3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1)
+{
+    Console.WriteLine("Число " + number + " является: нечетным");
+}
+else
+{
+    Console.WriteLine("Число " + number + "является: четным");
+}
